6e9a3f4aa47dd46bea9b0f9dc6228e9e1343dc9a
mfrade13 <m1gu3lff@gmail.com>
2025-04-01 20:33:03 -0400

    Avance de eventos en la clase

--- Clases/Clase_transiciones/main.lua
+++ Clases/Clase_transiciones/main.lua
@@ -8,10 +8,12 @@ local CW = display.contentWidth
 local CH = display.contentHeight
 
 local transicion_titulo
+local frutas = {}
 local fondo = display.newImageRect("bg_3.jpg", CW, CH)
 --fondo.anchorX = 0; fondo.anchorY = 0
 fondo.x = CW/2
 fondo.y = CH/2
+fondo.name = "Fondo"
 
 local fruta = display.newImageRect("fruta.png", 100, 100)
 fruta.x = 100
@@ -21,10 +23,12 @@ print(fruta.alpha)
 --fruta.alpha= 0
 --fruta:setFillColor(0, 1)
 fruta.rotation = 720
+fruta.name = "manzana"
 
 local titulo = display.newText("Game", CW/2, CH/6, native.systemFont, 55)
 print(titulo.size)
 titulo.size = 30; titulo.alpha= 0.2
+titulo.name = "titulo"
 
 local btn = display.newImageRect("btn_play.png", 100, 100)
 btn.x = CW/2
@@ -39,6 +43,7 @@ end
 
 function repetir()
     print("estamos repitiendo iteraciones durante la transicion")
+    print(fruta.x)
 end
 transicion_titulo = transition.to(titulo, {time=1000, size = 90, alpha = 1, iterations = 5, tag="crecer", onRepeat=repetir})
 transition.to(fruta, {time=3000, x=600, y = 500, rotation=1200, transition=easing.inBack, onComplete=moverFruta})
@@ -63,12 +68,65 @@ function btn:touch( event)
         print("Fase moved")
 
     elseif event.phase == "ended" then
-        print("Fase ended")
+        print("Fase ended boton")
         self.x = event.x
         self.y = event.y
         transition.resume("crecer")
+        self.isVisible = false
+        fondo.alpha = 0.1
+        for i=1, #frutas do
+            frutas[i].alpha = 0.3
+        end
     end
+    return 1
 end
 
+function removerFruta(self, event)
+    if event.phase == "began" then
+        print("Fase bega")
+    elseif event.phase == "moved" then
+        print("Fase moved")
+    elseif event.phase == "ended" then
+        print("Fruta removida: " .. self.name)
+        if self.name == "Fondo" then
+            self.alpha = 0.5
+        else
+            self:removeSelf()
+            self = nil
+        end
+    end
+    return 1
+end
+
+function frutasRemovidas( self, event )
+    if event.phase == "ended" then
+        print("Fruta removida: ")
+        if self.alpha == 1 then
+            display.remove(self)
+        end
+    end
+   return 1
+end
+
+
+for i = 1,10 do
+    local fruta = display.newImageRect("fruta.png", 100, 100)
+    fruta.x = math.random(100, CW-100)
+    fruta.y = math.random(100, CH-100)
+    print(fruta.x, fruta.y)
+
+    fruta.touch = frutasRemovidas
+    fruta:addEventListener("touch", fruta)
+    frutas[i] = fruta
+end
+
+print(frutas[6].x)
+
+fruta.touch = removerFruta
+fondo.touch = removerFruta
+titulo.touch = removerFruta
+titulo:addEventListener("touch", titulo)
+fruta:addEventListener("touch", fruta)
+fondo:addEventListener("touch", fondo)
 btn:addEventListener("touch", btn)
 --btn:addEventListener("touch", resumir)
