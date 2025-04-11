aebd6bf9b4881b3a7cf72c16e77045d3da3209e6
mfrade13 <m1gu3lff@gmail.com>
2025-04-10 21:33:55 -0400

    Codigo de manejo de transformadas para el comic

--- Clase_Comic/page1.lua
+++ Clase_Comic/page1.lua
@@ -10,8 +10,35 @@ local CW = display.contentWidth
 local CH = display.contentHeight
 local ACW = display.actualContentWidth
 local ACH = display.actualContentHeight
- 
- 
+local fondo
+local indice = 1
+
+local posiciones = {
+    {xScale = 1, yScale=1, x=0,y=0},
+    {xScale = 1.48, yScale=3.1, x=0,y=0},
+    {xScale=3, yScale=3.1,x=-2050, y=0}
+}
+
+function moverAdelante(e)
+    if e.phase == "ended" then 
+        print(e.target.valor)
+        if indice >= 0 and indice<=#posiciones then
+            indice = indice + e.target.valor
+            print(posiciones[indice].xScale, posiciones[indice].yScale, 
+                            posiciones[indice].x, posiciones[indice].y  )
+            transition.to(fondo, {
+                time =1000, 
+                xScale=posiciones[indice].xScale, 
+                yScale=posiciones[indice].yScale,
+                x=posiciones[indice].x,
+                y=posiciones[indice].y
+            }
+            )
+        end
+    end    
+end
+
+
  
 -- -----------------------------------------------------------------------------------
 -- Scene event functions
@@ -23,11 +50,31 @@ function scene:create( event )
     local sceneGroup = self.view
     -- Code here runs when the scene is first created but has not yet appeared on screen
     print("estoy en la pagina 1")
-    local fondo = display.newImageRect(sceneGroup,"spiderman2.jpg", CW, CH)
+    fondo = display.newImageRect(sceneGroup,"spiderman2.jpg", CW, CH)
     fondo.anchorX = 0; fondo.anchorY = 0
 
-    fondo:scale(1.48,3.1)
+    local izquierda = display.newImageRect(sceneGroup,"Flecha_izquierda.png", 385, 503)
+    izquierda.x = CW/2 - 100
+    izquierda.y = CH -150
+    izquierda.xScale=0.3; izquierda.yScale=0.3
+    izquierda.anchorX = 0; izquierda.anchorY = 0
+    izquierda.valor = -1
 
+    local derecha = display.newImageRect(sceneGroup,"Flecha_derecha.png", 385, 503)
+    derecha.x = CW/2 + 100
+    derecha.y = CH -150
+    derecha.xScale=0.3; derecha.yScale=0.3
+    derecha.anchorX = 0; derecha.anchorY = 0
+    derecha.valor = 1
+    izquierda:addEventListener("touch", moverAdelante)
+    derecha:addEventListener("touch", moverAdelante)
+    -- print(fondo.width, fondo.height)
+    local panel_derecho = display.newRect(sceneGroup,CW,0,50,CH)
+    panel_derecho:setFillColor(0.4)
+    panel_derecho.anchorX=1; panel_derecho.anchorY=0
+    fondo:scale(3.3,3.1)
+    -- print(fondo.contentWidth, fondo.height)
+    fondo:translate(0,- 1/3 * fondo.contentHeight)
 end
  
  
