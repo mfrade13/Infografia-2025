c33677e2bb1843430ec675ef18227965f071d760
mfrade13 <m1gu3lff@gmail.com>
2025-04-15 10:32:24 -0400

    Actualizacion de la clase de fisica

--- Clases/Clase_Fisica/main.lua
+++ Clases/Clase_Fisica/main.lua
@@ -29,11 +29,36 @@ local sequence =     {
 }
 local cazador = display.newSprite(sprite_sheet_1, sequence)
 cazador.x = CW/2
-cazador.y = CH/2
+cazador.y = CH/4
 cazador:play()
+
+local box_options = {
+    halfWidth = CW/2,
+    halfHeight = 100/2,
+    x=0,y=0,angle=0
+}
+
+local box_cazador = {
+    halfWidth = 20,
+    halfHeight = 100,
+    x=0,y=0,angle=0
+}
+
+local piso = display.newImageRect("piso.png", 580, 252)
+piso.x = CW/2
+piso.y = CH - 100
+
+local f1 = display.newImageRect("fruta.png", 100, 100)
+f1.x = CW/2 -25
+f1.y = CH -200
+
 
 physics.start()
 print(physics.getGravity())
-physics.setDrawMode("debug")
-physics.addBody(cazador, "dynamic", {radius =100})
-physics.setGravity(0, 0)
+physics.setDrawMode("hybrid")
+physics.addBody(cazador, "dynamic", {box =box_cazador,density=2, bounce = 0.2})
+cazador.isFixedRotation = false
+--physics.setGravity(0, 0)
+physics.addBody(piso, "static", {box = box_options, bounce = 0.2})
+print(piso.bodyType.bounce)
+physics.addBody(f1, "kinematic", {radius=50, bounce = 0.2 })
